3f367136517d59956753e25a3528510334dece43
ktadique <117470787+ktadique@users.noreply.github.com>
2023-01-08 00:52:26 +0000

    started working on utility function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,118 +1,197 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+//========================================================================
+//Password generator Psuedocode
+//------------------------------------------------------------------------
+//user clicks generate password
 
+//generator prompts options:
+//  password length
+//   prompts for input
+//    password length must be between 10-64
+//    parse input into a number
+//    if not error message (must be at least 10/can not be  more than 64)
+// Validate:
+//  lowercase
+// Validate:
+//  uppercase
+// Validate:
+//  numerical
+// Validate:
+//  special characters
+//
+//    if none are selected another error message
+
+//  use math.random on all the options to randomly select a character
+//    use math.random to randomly position selected random characters
+
+//generator creates password based on the selected options
+
+//create a jumbler at the end to randomize the output string
+//========================================================================
+
+let lengthInput = 0;
+let passLowerCase;
+let passUpperCase;
+let passNumeric;
+let passSpecial;
+
+function getPasswordLength() {
+  // do...while = executes the body of the loop first, then the while condition is evaluated
+  do {
+    //parseint ensures that the input will always be a number
+    lengthInput = parseInt(
+      prompt(
+        "How many characters would you like your password to contain? (Must be between 10-64)"
+      )
+    );
+    if (lengthInput < 10 || lengthInput > 64) {
+      alert("Password must be between 10-64 characters long.");
+    }
+  } while (lengthInput < 10 || lengthInput > 64);
+  //if above is true, the body of do statement is executed again
+  //if not, loop stops.
+
+  /*   
+  //makes sure that the while loop runs at least once
+  lengthInput = 0;
+  while (lengthInput < 10 || lengthInput > 64) {
+    //parseint ensures that the input will always be a number
+    lengthInput = parseInt(
+      prompt(
+        "How many characters would you like your password to contain? (Must be between 10-64)"
+      )
+    );
+    if (lengthInput < 10 || lengthInput > 64) {
+      alert("Password must be between 10-64 characters long.");
+    }
+  }*/
 }
+
+// Function to prompt user for password options
+function getPasswordOptions() {}
+
+// if (passLength >= 10 && passLength <= 64) {
+//   return passLength;
+// } else {
+//   alert("Password must be up to 10-64 characters long.");
+// }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+  // get a random index value from an array
+  let randomIndex = Math.floor(Math.random() * arr.length);
+  // get a random character
+  let randomCharacter = arr[randomIndex];
 
+  return randomCharacter;
 }
 
 // Function to generate password with user input
-function generatePassword() {
 
+function generatePassword() {
+  getPasswordLength();
 }
 
+//DO NOT TOUCH
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
